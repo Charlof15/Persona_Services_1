75a967486ba3e82ae9415548ceff8c1da4436ade
Karla Camacho <karlacamacho535.d@gmail.com>
2021-12-21 12:55:39 -0600

    @Data en model
    Uso de Lombok

--- src/main/java/com/ffm/persona/controller/Controller.java
+++ src/main/java/com/ffm/persona/controller/Controller.java
@@ -15,6 +15,8 @@ import org.springframework.web.server.ResponseStatusException;
 import com.ffm.persona.model.Personas;
 import com.ffm.persona.service.ServicePersona;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class Controller {
 
@@ -22,16 +24,19 @@ public class Controller {
 	private ServicePersona servicePersonas;
 
 	@GetMapping(path = "/personas")
+	@ApiOperation(value = "CONSULTAR TODAS LAS PERSONAS", notes = "Detalle de las personas registradas")
 	public Object ObteniendoPersonas() {
 		return servicePersonas.getAll();
 	}
 
 	@GetMapping(path = "/personas/idPersona/{idPersona}")
+	@ApiOperation(value = "CONSULTAR TODAS LAS PERSONAS POR MEDIO DE ID", notes = "Es necesario el id para encontrar una persona")
 	public Object busquedaPersonaPorId(@PathVariable(value = "idPersona") Integer idPersona) {
 		return servicePersonas.getIdPersona(idPersona);
 	}
 
 	@DeleteMapping(path = "/personas/idPersona/{idPersona}")
+	@ApiOperation(value = "ELIMINAR UNA PERSONA", notes = "Es necesario insertar un id para la eliminación de una persona")
 	public Object eliminaPersona(@PathVariable(value = "idPersona") Integer idPersona) {
 		try {
 			return new ResponseEntity<>(servicePersonas.deletePersona(idPersona), HttpStatus.OK);
@@ -41,6 +46,7 @@ public class Controller {
 	}
 
 	@PostMapping(path = "/personas")
+	@ApiOperation(value = "CREAR UNA PERSONA", notes = "Registrar una nueva persona")
 	public Object creaPersona(@RequestBody Personas personas) {
 		try {
 			return new ResponseEntity<>(servicePersonas.agregarPersona(personas), HttpStatus.OK);
@@ -50,6 +56,7 @@ public class Controller {
 	}
 
 	@PatchMapping(path = "/personas/idPersona/{idPersona}")
+	@ApiOperation(value = "ACTUALIZAR UN REGISTRO", notes = "Es necesario un id para la actualización del registro")
 	public Object actualizaPersona(@PathVariable(value = "idPersona") Integer idPersona,
 			@RequestBody Personas personas) {
 		return servicePersonas.actualizarPersonas(idPersona, personas);
--- src/main/java/com/ffm/persona/model/Personas.java
+++ src/main/java/com/ffm/persona/model/Personas.java
@@ -1,93 +1,23 @@
 package com.ffm.persona.model;
 
-public class Personas {
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
+
+@Data
+public class Personas {
+	
+	
 	Integer idPersona;
+	@ApiModelProperty(example = "Karla", name = "nombrePersona", notes = "Nombre de la persona que se creara", required = true)
 	String nombrePersona;
 	String apellidoPaterno;
 	String apellidoMaterno;
 	String telefonoCelular;
 	String correoElectronico;
 	String fechaRegistro;
+	@ApiModelProperty(example = "1",  notes = "Determina si el registro esta visible o no valor 1= activo 0 = inactivo", required = true)
 	String activo;
 
-	public Personas() {
-
-	}
-
-	public Personas(String nombrePersona, String apellidoPaterno, String apellidoMaterno, String telefonoCelular,
-			String correoElectronico) {
-		super();
-
-		this.nombrePersona = nombrePersona;
-		this.apellidoPaterno = apellidoPaterno;
-		this.apellidoMaterno = apellidoMaterno;
-		this.telefonoCelular = telefonoCelular;
-		this.correoElectronico = correoElectronico;
-	}
-
-	public Integer getIdPersona() {
-		return idPersona;
-	}
-
-	public void setIdPersona(Integer idPersona) {
-		this.idPersona = idPersona;
-	}
-
-	public String getNombrePersona() {
-		return nombrePersona;
-	}
-
-	public void setNombrePersona(String nombrePersona) {
-		this.nombrePersona = nombrePersona;
-	}
-
-	public String getApellidoPaterno() {
-		return apellidoPaterno;
-	}
-
-	public void setApellidoPaterno(String apellidoPaterno) {
-		this.apellidoPaterno = apellidoPaterno;
-	}
-
-	public String getApellidoMaterno() {
-		return apellidoMaterno;
-	}
-
-	public void setApellidoMaterno(String apellidoMaterno) {
-		this.apellidoMaterno = apellidoMaterno;
-	}
-
-	public String getTelefonoCelular() {
-		return telefonoCelular;
-	}
-
-	public void setTelefonoCelular(String telefonoCelular) {
-		this.telefonoCelular = telefonoCelular;
-	}
-
-	public String getCorreoElectronico() {
-		return correoElectronico;
-	}
-
-	public void setCorreoElectronico(String correoElectronico) {
-		this.correoElectronico = correoElectronico;
-	}
-
-	public String getFechaRegistro() {
-		return fechaRegistro;
-	}
-
-	public void setFechaRegistro(String fechaRegistro) {
-		this.fechaRegistro = fechaRegistro;
-	}
-
-	public String getActivo() {
-		return activo;
-	}
-
-	public void setActivo(String activo) {
-		this.activo = activo;
-	}
 }
 	
